f5e2cf72d862520d3598d767edd1dd814514d786
cheran <gcharankumar556@gmail.com>
2023-12-22 16:14:34 +0530

    largest

--- largest.py
+++ largest.py
@@ -1,3 +1,9 @@
-numbers=[1,9,11,23,65,78,43]
-maxnumbers=[i for i in numbers if i==max(numbers)]
-print(*maxnumbers,sep=',')
+numbers=[1,9,11,23,65,78,143]
+out=numbers[0]
+out2=numbers[0]
+for var in numbers:
+    if out<var:
+        for var in numbers:
+            if out2<var and var!=out:
+                out=var
+print(out2)
